e1754fdea7cc8e2adba462b97008d1ce0eb1a2ac
AlFeVval <88679162+AlFeVval@users.noreply.github.com>
2021-08-19 13:57:28 -0500

    Actualización
    Se ha arreglado el error de que arrojaba basura, y es casi el archivo final

--- main.asm
+++ main.asm
@@ -1,24 +1,23 @@
-;This is a template with the most typical sections of an assembly-language program
-;This program keeps an LED turned on while the PIC is biased
-;The LED is attached to the pin 0 of PORTB
-;The instruction set is used with a=0 (Access RAM)
-;******************Header Files******************************
-    list	    p=18f4550        ; list directive to define processor
-    #include	    "p18f4550.inc"
-    
+
+; PIC18F4550 Configuration Bit Settings
+
+; Assembly source line config statements
+
+#include "p18f4550.inc"
+
 ; CONFIG1L
-  CONFIG  PLLDIV = 5            ; PLL Prescaler Selection bits (Divide by 5 (20 MHz oscillator input))
-  CONFIG  CPUDIV = OSC3_PLL4    ; System Clock Postscaler Selection bits ([Primary Oscillator Src: /3][96 MHz PLL Src: /4])
-  CONFIG  USBDIV = 2            ; USB Clock Selection bit (used in Full-Speed USB mode only; UCFG:FSEN = 1) (USB clock source comes from the 96 MHz PLL divided by 2)
+  CONFIG  PLLDIV = 1            ; PLL Prescaler Selection bits (No prescale (4 MHz oscillator input drives PLL directly))
+  CONFIG  CPUDIV = OSC1_PLL2    ; System Clock Postscaler Selection bits ([Primary Oscillator Src: /1][96 MHz PLL Src: /2])
+  CONFIG  USBDIV = 1            ; USB Clock Selection bit (used in Full-Speed USB mode only; UCFG:FSEN = 1) (USB clock source comes directly from the primary oscillator block with no postscale)
 
 ; CONFIG1H
-  CONFIG  FOSC = HSPLL_HS       ; Oscillator Selection bits (HS oscillator, PLL enabled (HSPLL))
+  CONFIG  FOSC = INTOSC_HS      ; Oscillator Selection bits (Internal oscillator, HS oscillator used by USB (INTHS))
   CONFIG  FCMEN = OFF           ; Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
   CONFIG  IESO = OFF            ; Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
 
 ; CONFIG2L
-  CONFIG  PWRT = ON             ; Power-up Timer Enable bit (PWRT enabled)
-  CONFIG  BOR = OFF             ; Brown-out Reset Enable bits (Brown-out Reset disabled in hardware and software)
+  CONFIG  PWRT = OFF            ; Power-up Timer Enable bit (PWRT disabled)
+  CONFIG  BOR = ON              ; Brown-out Reset Enable bits (Brown-out Reset enabled in hardware only (SBOREN is disabled))
   CONFIG  BORV = 3              ; Brown-out Reset Voltage bits (Minimum setting 2.05V)
   CONFIG  VREGEN = OFF          ; USB Voltage Regulator Enable bit (USB voltage regulator disabled)
 
@@ -27,14 +26,14 @@
   CONFIG  WDTPS = 32768         ; Watchdog Timer Postscale Select bits (1:32768)
 
 ; CONFIG3H
-  CONFIG  CCP2MX = OFF          ; CCP2 MUX bit (CCP2 input/output is multiplexed with RB3)
-  CONFIG  PBADEN = ON          ; PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
+  CONFIG  CCP2MX = ON           ; CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+  CONFIG  PBADEN = OFF          ; PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
   CONFIG  LPT1OSC = OFF         ; Low-Power Timer 1 Oscillator Enable bit (Timer1 configured for higher power operation)
   CONFIG  MCLRE = ON            ; MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
 
 ; CONFIG4L
   CONFIG  STVREN = OFF          ; Stack Full/Underflow Reset Enable bit (Stack full/underflow will not cause Reset)
-  CONFIG  LVP = OFF             ; Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+  CONFIG  LVP = ON              ; Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
   CONFIG  ICPRT = OFF           ; Dedicated In-Circuit Debug/Programming Port (ICPORT) Enable bit (ICPORT disabled)
   CONFIG  XINST = OFF           ; Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
 
@@ -68,15 +67,17 @@
 ; CONFIG7H
   CONFIG  EBTRB = OFF           ; Boot Block Table Read Protection bit (Boot block (000000-0007FFh) is not protected from table reads executed in other blocks)
 
+
 ;****************Variables Definition*********************************
-    TEMP		EQU	0x22			;Reservamos 22 localidades de memoria
-    CONSTANT		MASK = b'00001111'		;Mascara que delimitara los bits mas significativos
+    TEMP		EQU	0x50			;Reservamos 22 localidades de memoria
+    CONSTANT		MASK = b'11110000'		;Mascara que delimitara los bits mas significativos
 ;****************Main code*****************************
 			ORG     0x000             	;reset vector
   			GOTO    MAIN              	;go to the main routine
 INITIALIZE:
 			CLRF	TRISD			;Puerto D como salidas
-			SETF	TRISC			;Puerto C como entradas
+			ADPCFG = 0x01FF			;Puerto B trabaje como entradas digitales
+			SETF	TRISB			;Todo puerto B como entrada
 			
 			RETURN				;end of initialization subroutine
 
@@ -84,11 +85,17 @@ MAIN:
 			CALL 	INITIALIZE
 
 BASE:
-			MOVF	PORTC, 0		;PORTC es movido a WREG
-			ANDLW	MASK			;
-			MOVWF	TEMP			;
-			CALL	DECODE		;
-			MOVWF	PORTD			;
+			MOVF	PORTB, 0		;PORTB es movido a WREG
+			ANDLW	MASK			;Aplicamos mascara para los bits mas significativos
+			MOVWF	TEMP			;Actualizamos el valor ya delimitado en temporal
+			RRNCF	TEMP,W	   		;Corremos un bit de dicho valor
+			MOVWF	TEMP			;Nueva actualizacion
+			RRNCF	TEMP,W	   		;Nuevo Corrimient
+			MOVWF	TEMP			;Nueva actualizacion
+			RRNCF	TEMP,W	   		;Ultimo corrimiento
+			MOVWF	TEMP			;Ultima actualizacion
+			CALL	DECODE			;Llamamos la función de decodificación
+			MOVWF	PORTD			;Muestra el resultado codificado
 			GOTO 	BASE			;infinite loop
 
 DECODE:
@@ -99,7 +106,7 @@ DECODE:
 			RETLW	b'01111001'		;//3
 			RETLW	b'00110011'		;//4
 			RETLW	b'01011011'		;//5
-			RETLW	b'00011111'		;//6
+			RETLW	b'01011111'		;//6
 			RETLW	b'01110000'		;//7
 			RETLW	b'01111111'		;//8
 			RETLW	b'01110011'		;//9
@@ -109,5 +116,5 @@ DECODE:
 			RETLW	b'00111101'		;//D
 			RETLW	b'01001111'		;//E
 			RETLW	b'01000111'		;//F
-			
+
 			END                       	;end of the main program
